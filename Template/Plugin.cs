d99e1a3af7fb4a94efabb5bc66219011a4de1f63
Matthew Pelletier <matthewp227@gmail.com>
2025-04-17 14:18:58 -0400

    Made weight go up by the picked up item.

--- Template/Plugin.cs
+++ Template/Plugin.cs
@@ -38,5 +38,6 @@ public class Plugin : BaseUnityPlugin
     {
         _harmony.PatchAll(typeof(ShipLightsPatch));
         _harmony.PatchAll(typeof(PlayerControllerBPatch));
+        _harmony.PatchAll(typeof(BeltBagItemPatch));
     }
 }
---
+++ Template/patch/BeltBagItemPatch.cs
@@ -0,0 +1,26 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace YourThunderstoreTeam.patch;
+
+[HarmonyPatch(typeof(BeltBagItem))]
+
+public class BeltBagItemPatch
+    {
+    [HarmonyPatch(nameof(BeltBagItem.PutObjectInBagLocalClient))]
+    [HarmonyPrefix]
+    private static bool OnAddToBag(ref BeltBagItem __instance,object[] __args)
+    {
+        
+        GrabbableObject grabbableObject = (GrabbableObject)__args[0];
+        HUDManager.Instance.AddTextToChatOnServer("Adding to bag Weight:" + grabbableObject.itemProperties.weight);
+        __instance.itemProperties.weight += grabbableObject.itemProperties.weight - 1;
+        
+        return true;
+    }
+}
+
